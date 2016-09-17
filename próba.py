5b35cae0fcc80da0309b7bf417acb21c10419fe2
handriss <andrashinkel@gmail.com>
2016-09-17 09:11:34 +0200

    yo rompere tus fotos

--- próba.py
+++
@@ -1,7 +0,0 @@
-import time
-
-while True:
-    if int(round(time.time() * 1000)) % 500 == 0:
-        print(int(round(time.time() * 1000)))
-    # millis = int(round(time.time() * 1000))
-    # print(millis)
